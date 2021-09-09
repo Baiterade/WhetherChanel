d2f26ed8e995a3b27d43332d840623054fe49d07
Jager O <44758792+Baiterade@users.noreply.github.com>
2021-09-08 22:15:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $("#searchButton").on("click", function () {
     
     console.log(cityInput.value);
 
-    fetch('http://open.mapquestapi.com/geocoding/v1/address?key=roGqv4kTHPoSVNTzzl7cTbyMNx2B7A5X&location=' + cityInput.value)
+    fetch('https://open.mapquestapi.com/geocoding/v1/address?key=roGqv4kTHPoSVNTzzl7cTbyMNx2B7A5X&location=' + cityInput.value)
         
         .then(response => {
             return response.json();
@@ -34,14 +34,14 @@ function update(){
             $("#currentWind").text("Wind (MPH): " + data.current.wind_speed)
             $("#currentHum").text("Humidity (%): " + data.current.humidity)
             $("#currentUV").text("UV Index: " + data.current.uvi)
-            $("#currentIcon").attr("src", "http://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png")
+            $("#currentIcon").attr("src", "https://openweathermap.org/img/wn/" + data.current.weather[0].icon + "@2x.png")
 
             $(".forecast").empty();
 
             for(i = 1; i < 6; i++){
                 var newDay = document.createElement("div");
                 $(newDay).addClass("card col p-3 m-3");
-                $(newDay).append("<img class = 'bg-secondary rounded' src = 'http://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png' width=50 height=50 >");
+                $(newDay).append("<img class = 'bg-secondary rounded' src = 'https://openweathermap.org/img/wn/" + data.daily[i].weather[0].icon + "@2x.png' width=50 height=50 >");
                 $(newDay).append("<li>Temp: " + data.daily[i].temp.day + "</li>");
                 $(newDay).append("<li>Wind: " + data.daily[i].wind_speed + "</li>");
                 $(newDay).append("<li>Humidity: " + data.daily[i].humidity + "</li>");
